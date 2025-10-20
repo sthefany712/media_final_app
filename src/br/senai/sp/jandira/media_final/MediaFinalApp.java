637099d359d5b11a322816312d8a7d5571cd119d
STHEFANY DA SILVA CARMO CORREIA <25203734@CFP127ALUNO.LOCAL>
2025-10-20 11:27:50 -0300

    interface gráfica

--- src/br/senai/sp/jandira/media_final/MediaFinalApp.java
+++ src/br/senai/sp/jandira/media_final/MediaFinalApp.java
@@ -1,4 +1,102 @@
 package br.senai.sp.jandira.media_final;
 
-public class MediaFinalApp {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+//extends == é uma (herança)
+public class MediaFinalApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args); //chamo o launch no main e o launch chama o start, como? n importa
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        //starta a criação da tela. ele importa um parametro: stage stage == tela, palco sem nada.
+        //"as cortinas vão se abrir"
+        //override como se fosse um postite, o start é obrigatório para iniciar o que vc quer aplicar. Eu n quero
+        //oq a herança esta me dando mas o que eu estou "ordenando".
+        //Determinar o tamanho do stage
+        stage.setWidth(600); //largura //stage PAI DE TODOS
+        stage.setHeight(500);//altura
+
+        //Determinar o título do stage (tela/janela)
+        stage.setTitle("Média Final");
+
+        //Painel Raiz (root) ele é o primeiro componente, componente pai.
+        //costumamos colocar root (poderia ser qualquer outra coisa, mas n faria sentido)
+        BorderPane root = new BorderPane();
+
+        Label labelTitulo = new Label(); //rótulo
+        labelTitulo.setText("Escola \"Prof. Vicente Amato\""); // usa \ \ para as "" aparecerem na tela
+        //Formatação do texto da Label
+        //propriedade css abaixo (sempre olhar documentação) todos começam com fx p/ diferenciar o java do css
+        labelTitulo.setStyle("-fx-text-fill: #005AFF;-fx-font-size: 32;-fx-font-weight: bold;");
+        labelTitulo.setPadding(new Insets(10, 0, 10, 10)); //sentido horário
+
+         // esse Scene precisa que fale p/ ele quem é o pai(parent) dele
+        Scene scene = new Scene(root); //criei a cena
+        stage.setScene(scene); //joguei no palco
+
+        // Painel de resultados - Parte de baixo
+        VBox painelResultado = new VBox(); //caixa que deixa na vertical.
+        painelResultado.setPadding(new Insets(0, 0, 10, 10));
+        Label labelAluno = new Label("Nome do aluno: "); //construtor
+        Label labelMedia = new Label("Média Final: "); //construtor
+        Label labelSituacao = new Label("Situação: "); //construtor
+        painelResultado.getChildren().addAll(labelAluno, labelMedia, labelSituacao);
+
+        // Painel de Botões:
+        VBox painelDeBotoes = new VBox(); //new == algo novo
+        painelDeBotoes.setPadding(new Insets(0, 10, 10, 0));
+        painelDeBotoes.setSpacing(10); //spacing estou dizendo q todos os compon. filhos vão ter tal espassamento entre eles
+        Button buttonCalcularMedia = new Button("Calcular Media"); //botões
+        buttonCalcularMedia.setPrefWidth(150);
+        buttonCalcularMedia.prefHeight(50);
+        Button buttonLimpar = new Button("Limpar");
+        buttonLimpar.setPrefWidth(150);
+        buttonLimpar.prefHeight(50);
+        Button buttonSair = new Button("Sair");
+        buttonSair.setPrefWidth(150);
+        buttonSair.prefHeight(50);
+        painelDeBotoes.getChildren().addAll(buttonCalcularMedia, buttonLimpar, buttonSair);
+
+        //Painel formulário
+        VBox painelFormulario = new VBox();
+        painelFormulario.setPadding(new Insets(0, 0, 10,10));
+        Label labelNome = new Label("Nome do aluno:");
+        Label labelNota1 = new Label("Nota 1:");
+        Label labelNota2= new Label("Nota 2:");
+        Label labelNota3= new Label("Nota 3:");
+        Label labelNota4= new Label("Nota 4:");
+        TextField textFieldNome = new TextField();
+        TextField textFieldNota1 = new TextField();
+        TextField textFieldNota2= new TextField();
+        TextField textFieldNota3= new TextField();
+        TextField textFieldNota4= new TextField();
+        painelFormulario.getChildren().addAll(
+                labelNome, textFieldNome,
+                labelNota1, textFieldNota1,
+                labelNota2, textFieldNota2,
+                labelNota3, textFieldNota3,
+                labelNota4, textFieldNota4
+        );
+        //root == é a raiz do projeto, o borderPane, depois escolhe onde vai colocar e dentro das () chama oq vai aparecer
+        root.setTop(labelTitulo);
+        root.setBottom(painelResultado);
+        root.setRight(painelDeBotoes);
+        root.setLeft(painelFormulario);
+
+        //Mostrar a stage(tela)
+        stage.show(); //sempre fica por último
+
+        //Eventos de clique dos botões
+        buttonCalcularMedia.addEventHandler(""); //manipulador de eventos
+    }
 }
